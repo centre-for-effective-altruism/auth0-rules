046e7287b4d45c294990e6eed13ffcb2a5c46677
JP Addison <johnpaddison@gmail.com>
2021-04-27 14:41:57 +0100

    still arguing with who controls the configuration global object

--- scripts/types/globals.d.ts
+++ scripts/types/globals.d.ts
@@ -3,6 +3,16 @@ import { User } from 'auth0'
 declare global {
   /** We inject this into the top of functions, so it will be present */
   const TEMPLATE_DATA: Record<string, any>
+  /** @tepez/auth0-rules-types declares that the global configuration object is of this type, so we extend it for all uses of configuration, including those in db scripts */
+  interface IAuth0RuleConfiguration {
+    POSTGRES_USERNAME: string
+    POSTGRES_PASSWORD: string
+    POSTGRES_HOST: string
+    POSTGRES_DATABASE: string
+    POSTGRES_PORT?: string
+    AUTH0_CLIENT_ID: string
+    AUTH0_CLIENT_SECRET: string
+  }
 
   // --- DB Globals ---
   /** Parfit DB Person */
@@ -24,16 +34,6 @@ declare global {
   }
   /** Signature of Database Action Script callback */
   type DbScriptCallback = (error: Error | null, person?: CallbackUser) => void
-  /** Configured in Auth0 UI */
-  const configuration: {
-    POSTGRES_USERNAME: string
-    POSTGRES_PASSWORD: string
-    POSTGRES_HOST: string
-    POSTGRES_DATABASE: string
-    POSTGRES_PORT?: string
-    AUTH0_CLIENT_ID: string
-    AUTH0_CLIENT_SECRET: string
-  }
   /** Global error class */
   class WrongUsernameOrPasswordError extends Error {}
   /** Return this in `login` if user authentication fails. Auth0 provides this as a global. */
