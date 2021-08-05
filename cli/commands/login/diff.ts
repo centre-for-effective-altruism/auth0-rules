fef75e33ca0a4e6427bcbf4d5dfe171e171f83ea
JP Addison <johnpaddison@gmail.com>
2021-08-05 17:56:57 +0100

    emit non-zero exit codes on errors

--- cli/commands/login/diff.ts
+++ cli/commands/login/diff.ts
@@ -5,23 +5,31 @@ import { printCodeDiff } from '../../lib/utils'
 import { diffLines } from 'diff'
 
 export default async function run() {
-  const branding = auth0.branding
-  let existingTemplate
   try {
-    existingTemplate = (await branding.getUniversalLoginTemplate()).body
-  } catch (err) {
-    if (err.statusCode === 404) {
-      existingTemplate = ''
-    } else {
-      throw err
+    const branding = auth0.branding
+    let existingTemplate
+    try {
+      existingTemplate = (await branding.getUniversalLoginTemplate()).body
+    } catch (err) {
+      if (err.statusCode === 404) {
+        existingTemplate = ''
+      } else {
+        throw err
+      }
     }
-  }
-  const newTemplate = (
-    await fs.readFile(path.join(__dirname, '../../../templates/login.liquid'))
-  ).toString()
-  const diff = diffLines(existingTemplate, newTemplate)
-  const upToDateTemplates = printCodeDiff([[{ name: 'Login' }, diff]], 'login')
-  if (upToDateTemplates.length) {
-    console.log('Login template is up-to-date')
+    const newTemplate = (
+      await fs.readFile(path.join(__dirname, '../../../templates/login.liquid'))
+    ).toString()
+    const diff = diffLines(existingTemplate, newTemplate)
+    const upToDateTemplates = printCodeDiff(
+      [[{ name: 'Login' }, diff]],
+      'login'
+    )
+    if (upToDateTemplates.length) {
+      console.log('Login template is up-to-date')
+    }
+  } catch (err) {
+    console.log(err)
+    process.exit(1)
   }
 }
