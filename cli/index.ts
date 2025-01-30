f534289baf9cc93fb7233d5302da04b05238c824
Will Howard <w.howard256+github@gmail.com>
2025-01-30 12:37:03 +0000

    Remove Rules code

--- cli/index.ts
+++ cli/index.ts
@@ -1,9 +1,6 @@
 import path from 'path'
 import program from 'commander'
 
-program.command('rules', 'manage Auth0 rules', {
-  executableFile: path.join(__dirname, 'commands/rules/index.js'),
-})
 program.command('actions', 'manage Auth0 actions', {
   executableFile: path.join(__dirname, 'commands/actions/index.js'),
 })
