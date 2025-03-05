835e6df27ecd0e251e8495e764c81986d749a078
Will Howard <w.howard256+github@gmail.com>
2025-03-05 15:34:40 +0000

    Migrate to using postgres instead of Mongo

--- scripts/db/src/get-user.ts
+++ scripts/db/src/get-user.ts
@@ -1,5 +1,10 @@
-import { MongoClient } from 'mongodb'
-import { CallbackUser, DbScriptCallback, ForumUser } from '../types/db-types'
+import {
+  CallbackUser,
+  DbConfiguration,
+  DbScriptCallback,
+  ForumUser,
+} from '../types/db-types'
+import { Client as PGClient, ConnectionConfig as PGConnectionConfig } from 'pg'
 
 // TODO: This is pretty copy-pasta-y from login. We should fix this by building
 // good code-sharing functionality into this repo. But notice that we can't just
@@ -26,44 +31,68 @@ async function getByEmail(email: string, callback: DbScriptCallback) {
   //     callback(new Error("my error message"));
   try {
     /** Get required dependencies */
-    const { MongoClient } = require('mongodb@4.1.0')
+    const { Client: PGClient } = require('pg@8.7.1')
 
-    const { MONGO_URI, MONGO_DB_NAME } = configuration
+    const {
+      POSTGRES_USERNAME,
+      POSTGRES_PASSWORD,
+      POSTGRES_HOST,
+      POSTGRES_DATABASE,
+      POSTGRES_PORT,
+    } = configuration
 
     /**
      * Logic in this function tries to match:
      * EAForum/packages/lesswrong/server/vulcan-lib/apollo-server/authentication.tsx
      */
     async function getForumUser(): Promise<CallbackUser | null> {
-      // Connect to the Forum DB
-      const mongoClient: MongoClient = new MongoClient(MONGO_URI)
-      await mongoClient.connect()
+      const pgConnectionInfo: PGConnectionConfig = {
+        user: POSTGRES_USERNAME,
+        password: POSTGRES_PASSWORD,
+        host: POSTGRES_HOST,
+        database: POSTGRES_DATABASE,
+        port: POSTGRES_PORT ? parseInt(POSTGRES_PORT) : 5432,
+        ssl: TEMPLATE_DATA.pgShouldSsl,
+      }
 
-      // Query the users collection for someone with our email
-      const matchingUsers = await mongoClient
-        .db(MONGO_DB_NAME)
-        .collection<ForumUser>('users')
-        .find({ 'emails.address': email })
-        .collation({ locale: 'en', strength: 2 })
-        .toArray()
+      /**
+       * NOTE: Temporary fix for Auth0 bug August 2022
+       * Should be reverted ASAP
+       *
+       * Update Nov 2022: Auth0 told me the bug is fixed so I tried
+       * reverting and it broke - the bug is very much not fixed :(
+       */
+      process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
-      await mongoClient.close()
+      const pgClient: PGClient = new PGClient(pgConnectionInfo)
+      await pgClient.connect()
 
-      if (!matchingUsers.length) {
+      const forumQuery = `
+        SELECT * FROM "Users"
+        WHERE EXISTS (
+          SELECT 1 FROM unnest(emails) AS email
+          WHERE LOWER(email) = LOWER($1)
+        )
+      `
+      const res = await pgClient.query<ForumUser>(forumQuery, [email])
+
+      await pgClient.end()
+
+      process.env.NODE_TLS_REJECT_UNAUTHORIZED = '1'
+
+      if (res.rows.length === 0) {
         return null
       }
-      if (matchingUsers.length > 1) {
+      if (res.rows.length > 1) {
         throw new Error('More than one user with this email address')
       }
-      const forumUser = matchingUsers[0]
+      const forumUser = res.rows[0]
 
-      // Which email did we find?
       const emailInfo = forumUser.emails.find(
         (e) => e.address.toLowerCase() === email.toLowerCase()
       )
       if (!emailInfo) {
-        // This should never happen, as they were returned by mongo because that
-        // field matched
+        // This should never happen, as they were returned because that field matched
         throw new Error(
           `User found by email ${email}, does not have that email`
         )
@@ -77,7 +106,6 @@ async function getByEmail(email: string, callback: DbScriptCallback) {
       }
     }
 
-    /** Give priority to Forum users, as the integration is newer and it has more users */
     const forumUser = await getForumUser()
     if (forumUser) {
       return callback(null, forumUser)
--- scripts/db/src/login.ts
+++ scripts/db/src/login.ts
@@ -1,6 +1,6 @@
 import { compare } from 'bcrypt'
 import { createHash as createHash_ } from 'crypto'
-import { MongoClient } from 'mongodb'
+import { Client as PGClient, ConnectionConfig as PGConnectionConfig } from 'pg'
 import { CallbackUser, DbScriptCallback, ForumUser } from '../types/db-types'
 
 /** Authenticates a user against existing user databases */
@@ -15,38 +15,66 @@ async function login(
     const { createHash } = require('crypto') as {
       createHash: typeof createHash_
     }
-    const { MongoClient } = require('mongodb@4.1.0')
+    const { Client: PGClient } = require('pg@8.7.1')
 
-    const { MONGO_URI, MONGO_DB_NAME } = configuration
+    const {
+      POSTGRES_USERNAME,
+      POSTGRES_PASSWORD,
+      POSTGRES_HOST,
+      POSTGRES_DATABASE,
+      POSTGRES_PORT,
+    } = configuration
 
     /**
      * Logic in this function tries to match:
      * EAForum/packages/lesswrong/server/vulcan-lib/apollo-server/authentication.tsx
      */
     async function loginForumUser(): Promise<CallbackUser | null> {
-      // Connect to the Forum DB
-      const mongoClient: MongoClient = new MongoClient(MONGO_URI)
-      await mongoClient.connect()
+      const pgConnectionInfo: PGConnectionConfig = {
+        user: POSTGRES_USERNAME,
+        password: POSTGRES_PASSWORD,
+        host: POSTGRES_HOST,
+        database: POSTGRES_DATABASE,
+        port: POSTGRES_PORT ? parseInt(POSTGRES_PORT) : 5432,
+        ssl: TEMPLATE_DATA.pgShouldSsl,
+      }
 
-      // Query the users collection for someone with our email
-      const matchingUsers = await mongoClient
-        .db(MONGO_DB_NAME)
-        .collection<ForumUser>('users')
-        .find({ 'emails.address': email })
-        .collation({ locale: 'en', strength: 2 })
-        .toArray()
+      /**
+       * NOTE: Temporary fix for Auth0 bug August 2022
+       * Should be reverted ASAP
+       *
+       * Update Nov 2022: Auth0 told me the bug is fixed so I tried
+       * reverting and it broke - the bug is very much not fixed :(
+       */
+      process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
-      await mongoClient.close()
+      /** Construct a postgres client and connect to the server */
+      const pgClient: PGClient = new PGClient(pgConnectionInfo)
+      await pgClient.connect()
 
-      if (!matchingUsers.length) {
+      /** Query the users table for someone with our email */
+      const forumQuery = `
+        SELECT * FROM users 
+        WHERE EXISTS (
+          SELECT 1 FROM unnest(emails) AS email
+          WHERE LOWER(email) = LOWER($1)
+        )
+      `
+      const res = await pgClient.query<ForumUser>(forumQuery, [email])
+
+      /** Close the connection */
+      await pgClient.end()
+
+      process.env.NODE_TLS_REJECT_UNAUTHORIZED = '1'
+
+      if (res.rows.length === 0) {
         return null
       }
-      if (matchingUsers.length > 1) {
+      if (res.rows.length > 1) {
         throw new Error('More than one user with this email address')
       }
-      const forumUser = matchingUsers[0]
+      const forumUser = res.rows[0]
 
-      // Check their password
       // Meteor hashed its passwords twice, once on the client and once again on
       // the server. To preserve backwards compatibility with Meteor passwords,
       // we do the same, but do it both on the server-side
@@ -61,19 +89,12 @@ async function login(
         return null
       }
 
-      // Just in case we were dumb and did a stupid manual operation in our
-      // overly-flexible database
-      if (!Array.isArray(forumUser.emails)) {
-        throw new Error('Expected emails field to be an array')
-      }
-
       // Which email did we find?
       const emailInfo = forumUser.emails.find(
         (e) => e.address.toLowerCase() === email.toLowerCase()
       )
       if (!emailInfo) {
-        // This should never happen, as they were returned by mongo because that
-        // field matched
+        // This should never happen, as they were returned because that field matched
         throw new Error(
           `User found by email ${email}, does not have that email`
         )
@@ -87,7 +108,6 @@ async function login(
       }
     }
 
-    /** Give priority to Forum users, as the integration is newer and it has more users */
     const forumUser = await loginForumUser()
     if (forumUser) {
       return callback(null, forumUser)
